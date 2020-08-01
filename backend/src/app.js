f310e84b5da86ca2f6312192eaa2a08d01cf1f65
Vitor Porto <vhpporto@gmail.com>
2020-08-01 14:28:19 -0700

    Upd

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,29 @@
 import "dotenv/config";
-
+import * as Sentry from "@sentry/node";
 import express from "express";
+import Youch from "youch";
 import path from "path";
+import cors from "cors";
 import routes from "./routes";
 import "./database";
+import sentryConfig from "./config/sentry";
+
+import "express-async-errors";
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       "/files",
@@ -23,6 +33,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const erros = await new Youch(err, req).toJSON();
+
+      console.log(erros);
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    "https://40eb4248aba64e6d8d0a4ff243882a06@o428660.ingest.sentry.io/5374296",
+};
